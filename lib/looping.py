2c8b2ac342fbb4a2a4f1b7ea33fa22c5ea4849c6
kibetbrian <brian.kibet2@student.moringaschool.com>
2023-08-16 15:22:43 -0400

    completed lab

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,23 @@
 
 def happy_new_year():
     # code goes here!
-    pass
-
+    for i in range(10, 0, -1):
+        print(i)
+    print("Happy New Year!")
+   
 def square_integers(int_list):
     # code goes here!
-    pass
+    int_list =[num **2 for  num in int_list]
+    return int_list
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
